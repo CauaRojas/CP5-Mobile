d51e48f7c080b5f31062619595618f8a52320a2c
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-10-04 09:44:29 -0300

    finished my part

--- app/src/main/java/br/com/opengroup/cp5/CadastroFragment.kt
+++ app/src/main/java/br/com/opengroup/cp5/CadastroFragment.kt
@@ -7,7 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import br.com.opengroup.cp5.database.DatabaseHelper
+import com.google.android.material.textfield.TextInputEditText
 
 
 class CadastroFragment : Fragment() {
@@ -30,6 +33,25 @@ class CadastroFragment : Fragment() {
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, estados)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner?.adapter = adapter
+        val btnSalvar = view.findViewById<Button>(R.id.btnSalvar)
+        val txtNome = view.findViewById<TextInputEditText>(R.id.txtNome)
+        val txtSobrenome = view.findViewById<TextInputEditText>(R.id.txtSobrenome)
+        val txtTelefone = view.findViewById<TextInputEditText>(R.id.txtTelefone)
+        val txtPeso = view.findViewById<TextInputEditText>(R.id.txtPeso)
+        btnSalvar.setOnClickListener {
+            val nome = txtNome.text.toString()
+            val sobrenome = txtSobrenome.text.toString()
+            val telefone = txtTelefone.text.toString()
+            val peso = txtPeso.text.toString().toDouble()
+            val estado = spinner.selectedItem.toString()
+            DatabaseHelper(requireContext()).insertUsuario(nome, telefone, sobrenome, estado, peso)
+            txtNome.text?.clear()
+            txtSobrenome.text?.clear()
+            txtTelefone.text?.clear()
+            txtPeso.text?.clear()
+            spinner.setSelection(0)
+            Log.i("database2", DatabaseHelper(requireContext()).selectUsuarios().toString())
+        }
 
         super.onViewCreated(view, savedInstanceState)
 
--- app/src/main/java/br/com/opengroup/cp5/ListarFragment.kt
+++ app/src/main/java/br/com/opengroup/cp5/ListarFragment.kt
@@ -9,25 +9,10 @@ import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import android.widget.ListAdapter
 import android.widget.ListView
+import br.com.opengroup.cp5.database.DatabaseHelper
 import br.com.opengroup.cp5.entities.Usuario
 
 class ListarFragment : Fragment() {
-    val usuarios: List<Usuario> = listOf(
-        Usuario(1, "João", "1999999", "Da silva", "SP", 92.0),
-        Usuario(2, "Maria", "1999999", "da silva", "SP", 92.0),
-        Usuario(3, "Pedro", "1999999", "da silva", "SP", 92.0),
-        Usuario(4, "José", "1999999", "da silva", "SP", 92.0),
-        Usuario(5, "Carlos", "1999999", "da silva", "SP", 92.0),
-        Usuario(6, "João", "1999999", "da silva", "SP", 92.0),
-        Usuario(7, "Maria", "1999999", "da silva", "SP", 92.0),
-        Usuario(8, "Pedro", "1999999", "da silva", "SP", 92.0),
-        Usuario(9, "José", "1999999", "da silva", "SP", 92.0),
-        Usuario(10, "Carlos", "1999999", "da silva", "SP", 92.0),
-        Usuario(11, "João", "1999999", "da silva", "SP", 92.0),
-        Usuario(12, "Maria", "1999999", "da silva", "SP", 92.0),        Usuario(13, "Pedro", "1999999", "da silva", "SP", 92.0),
-        Usuario(14, "José", "1999999", "da silva", "SP", 92.0),
-        Usuario(15, "Carlos", "1999999", "da silva", "SP", 92.0),
-    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,6 +23,7 @@ class ListarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val listView: ListView = view.findViewById(R.id.listView)
+        val usuarios = DatabaseHelper(requireContext()).selectUsuarios()
         listView.adapter = ArrayAdapter<Usuario>(
             requireContext(),
             android.R.layout.simple_list_item_1,
