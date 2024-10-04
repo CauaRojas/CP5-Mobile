eca8886a8481a9c822ada110cd6530011402c7d2
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-10-04 08:47:19 -0300

    added form

--- app/src/main/java/br/com/opengroup/cp5/CadastroFragment.kt
+++ app/src/main/java/br/com/opengroup/cp5/CadastroFragment.kt
@@ -1,13 +1,17 @@
 package br.com.opengroup.cp5
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 
 class CadastroFragment : Fragment() {
+    val estados = arrayOf("AC","AL","AM","AP","BA","CE","DF","ES","GO","MA","MG","MS","MT","PA","PB","PE","PI","PR","RJ","RN","RO","RR","RS","SC","SP","SE","TO")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,8 +20,19 @@ class CadastroFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_cadastro, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val spinner = view.findViewById<Spinner>(R.id.estadosSpinner)
+        Log.i("spinner", spinner.toString())
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, estados)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner?.adapter = adapter
+
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
--- app/src/main/java/br/com/opengroup/cp5/MainActivity.kt
+++ app/src/main/java/br/com/opengroup/cp5/MainActivity.kt
@@ -2,10 +2,20 @@ package br.com.opengroup.cp5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCadastro = findViewById<View>(R.id.btnCadastro)
+        val btnLista = findViewById<View>(R.id.btnLista)
+
+        btnCadastro.setOnClickListener {
+            val fragment = CadastroFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, fragment).commit()
+        }
+
     }
 }
